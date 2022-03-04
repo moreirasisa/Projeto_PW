627162fdf75b5e6930d7bb74ee786c6b429f4905
Isadora Moreira <moreirassisadora@gmail.com>
2022-03-04 14:34:22 -0300

    Criação do Create e Index dos produtos

--- Projeto_PW/Controllers/ProdutosController.cs
+++ Projeto_PW/Controllers/ProdutosController.cs
@@ -9,6 +9,10 @@ namespace Projeto_PW.Controllers
         public static List<ProdutoModel> lsProdutos = new List<ProdutoModel>();
         public IActionResult Index()
         {
+            ProdutoModel produto = new ProdutoModel();
+            produto.Descricao = "Mouse gamer";
+            produto.Codigo = 678;
+            lsProdutos.Add(produto);
             return View(lsProdutos);
         }
 
